cd004684694d8e7b360ef63b5b953efa180eb15d
arte-mon <acm.workbox@gmail.com>
2025-02-24 21:29:58 +0300

    initial project build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/jsgame.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+    ],
+  },
+};
